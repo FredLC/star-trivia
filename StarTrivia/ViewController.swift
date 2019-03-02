7d9c58e94bf550b6a9aba465ebe995454d8740b5
fredlc <fredlefevre@outlook.com>
2019-03-02 12:57:15 -0500

    Initial project setup

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Fred Lefevre on 2019-03-02.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
