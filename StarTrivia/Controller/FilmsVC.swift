39fce552005edd3f454f2384abad103621776e5b
fredlc <fredlefevre@outlook.com>
2019-03-03 18:33:07 -0500

    Setup UI for FilmsVC

--- StarTrivia/Controller/FilmsVC.swift
+++ StarTrivia/Controller/FilmsVC.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class FilmsVC: UIViewController, PersonProtocol {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var episodeLabel: UILabel!
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var producerLabel: UILabel!
+    @IBOutlet weak var releasedLabel: UILabel!
+    @IBOutlet weak var crawlLabel: UITextView!
+    @IBOutlet weak var previousButton: FadeEnabledButton!
+    @IBOutlet weak var nextButton: FadeEnabledButton!
+    
     var person: Person!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print(person.name)
+    }
+    
+    func setButtonState() {
+        
+    }
+    
+    @IBAction func previousButtonPressed(_ sender: Any) {
+        setButtonState()
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        setButtonState()
     }
 
 }
